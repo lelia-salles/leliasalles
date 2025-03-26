42633576aa0583b6cb599aa5d3d64999f0101e2f
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-03-26 11:17:24 -0300

    update

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Portfolio Loaded Successfully!");
 
-    // Seleciona o menu (supondo que tenha um ID 'menu')
     const menu = document.getElementById("menu");
 
     if (menu) {
-        // Cria a div do banner
         const banner = document.createElement("div");
-        banner.id = "banner";
+        banner.className = "banner"; 
         banner.innerHTML = `<img src="lelia linkedin.png" alt="Banner Portfolio Lelia Salles" style="width:100%; height:auto;">`;
 
-        // Insere o banner logo depois do menu
         menu.insertAdjacentElement("afterend", banner);
     } else {
         console.warn("Elemento com ID 'menu' não encontrado!");
     }
 });
+
 

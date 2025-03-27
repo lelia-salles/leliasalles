dbf42389852e2c69ffd4025d5e45e53bd0e82d6f
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-03-27 13:51:35 -0300

    adjust image about me

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (aboutImageContainer) {
             const img = document.createElement("img");
             img.id = "babout-image"; 
-            img.src = "img/Lelia Portfolio.png";
+            img.src = "img/Lelia Portfolio 250.17px-300px.png";
             img.alt = "Colagem Portfolio Lelia Salles";
             aboutImageContainer.appendChild(img);
         } else {
